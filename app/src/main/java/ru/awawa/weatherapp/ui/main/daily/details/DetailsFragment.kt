5cad1c4e9ad29beb2f03af0e1117d31f4bb1f25a
0awawa0 <mail.0awawa0@gmail.com>
2020-06-27 16:29:03 +0300

    Changing the daily data representation. RecyclerView element containing all the data seems to be too heavy. The better way is to add details fragment to show more data

---
+++ app/src/main/java/ru/awawa/weatherapp/ui/main/daily/details/DetailsFragment.kt
@@ -0,0 +1,10 @@
+package ru.awawa.weatherapp.ui.main.daily.details
+
+import androidx.fragment.app.Fragment
+import org.koin.android.ext.android.inject
+import ru.awawa.weatherapp.repo.WeatherRepo
+
+
+class DetailsFragment: Fragment() {
+
+}
